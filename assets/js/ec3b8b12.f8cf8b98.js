896c139cc0fa15f4a0583f60f28f4defb5deb6b8
owonie <chlehdnjs@gmail.com>
2024-11-13 23:29:38 +0900

    Updates

---
+++ assets/js/ec3b8b12.f8cf8b98.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2931],{3369:e=>{e.exports=JSON.parse('{"tag":{"label":"JD","permalink":"/blog/tags/jd","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/jd","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
